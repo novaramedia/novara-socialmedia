21d58f93a2582cdfe2abe5ef5faefb5290904a00
fraserwatt <fraserwatt1990@gmail.com>
2017-09-16 19:07:00 +0100

    Update 02_Buffer_02_StagingTable.sql

--- 02_Buffer_02_StagingTable.sql
+++ 02_Buffer_02_StagingTable.sql
@@ -3,7 +3,7 @@ BEGIN
 	DECLARE CurrentDate DATE;
     SET CurrentDate = CAST(current_date() AS DATE);
 	
-	CREATE TEMPORARY TABLE SocMediaStagingTable	(			RandID				INT,
+	CREATE TEMPORARY TABLE SocMediaStagingTable	(			RandID				DECIMAL,
 															PageID				INT,
                   											PageTitle			VARCHAR(1000),
                   											PageTypePrimary		INT,
@@ -12,8 +12,9 @@ BEGIN
 						                  					PostingDate			DATE,
                                                             NovaraArchive		INT
     											);
-	
+
 	CREATE TEMPORARY TABLE SocMediaStagingTable2	(		PageID				INT,
+															RandID 				DECIMAL,
                   											PageTitle			VARCHAR(1000),
                   											PageTypePrimary		INT,
 								                  			PageTypeSecondary	INT,
@@ -23,8 +24,8 @@ BEGIN
                                                             Platform			VARCHAR(100),
                                                             PostType			VARCHAR(100)
 												);
-	
-	CREATE TEMPORARY TABLE SocMediaStagingTable3	(		R					VARCHAR(1000),
+
+	CREATE TEMPORARY TABLE SocMediaStagingTable3	(		RandID				DECIMAL,
 															PageID				INT,
                   											PageTitle			VARCHAR(1000),
                   											PageTypePrimary		INT,
@@ -34,31 +35,31 @@ BEGIN
 						                  					NovaraArchive		INT,
                                                             Platform			VARCHAR(100),
                                                             PostType			VARCHAR(100)
-												);    
-	
+												);
+
 	INSERT INTO SocMediaStagingTable (RandID, PageID, PageTitle, PageTypePrimary, PageTypeSecondary, PostingDate, PublishDate, NovaraArchive)
-	SELECT		UUID() AS RandID,
+	SELECT		RAND() AS RandID,
 				ID AS PageID,
 				PageTitle,
 				PageTypePrimary,
 				PageTypeSecondary,
-				@CurrentDate AS PostingDate,
+				CAST(current_date() AS DATE) AS PostingDate,
 				PublishDate,
                 1 AS NovaraArchive
 	FROM		WebsitePages
 	WHERE 		PageTypePrimary != 828 AND PageTypeSecondary != 828
-	AND			PublishDate < CAST(date_add(current_date(), INTERVAL -50 DAY) AS DATE) 
-	AND			PublishDate > CAST(date_add(current_date(), INTERVAL -1 YEAR) AS DATE) 
+	AND			PublishDate < CAST(date_add(current_date(), INTERVAL -50 DAY) AS DATE)
+	AND			PublishDate > CAST(date_add(current_date(), INTERVAL -1 YEAR) AS DATE)
 	AND			IFNULL(LastPosted,'2000-01-01') < CAST(date_add(current_date(), INTERVAL -6 WEEK) AS DATE)
-	ORDER BY	UUID();
-    
+	ORDER BY	RAND();
+
     INSERT INTO SocMediaStagingTable2 (PageID, PageTitle, PageTypePrimary, PageTypeSecondary, PublishDate, PostingDate, NovaraArchive, Platform, PostType)
 	SELECT 		ID AS PageID,
 				PageTitle,
 				PageTypePrimary,
 				PageTypeSecondary,
 				PublishDate,
-				@CurrentDate AS PostingDate,
+				CAST(current_date() AS DATE) AS PostingDate,
 				0 AS NovaraArchive,
                 'Twitter',
                 'Repost'
@@ -66,14 +67,14 @@ BEGIN
 	WHERE 		PageTypePrimary != 828 AND PageTypeSecondary != 828
 	AND			DATEDIFF(PublishDate,CAST(current_date() AS DATE))  IN (-3, -6)
 	ORDER BY	PublishDate DESC;
-	
+
     INSERT INTO SocMediaStagingTable2 (PageID, PageTitle, PageTypePrimary, PageTypeSecondary, PublishDate, PostingDate, NovaraArchive, Platform, PostType)
 	SELECT 		ID AS PageID,
 				PageTitle,
 				PageTypePrimary,
 				PageTypeSecondary,
 				PublishDate,
-				@CurrentDate AS PostingDate,
+				CAST(current_date() AS DATE) AS PostingDate,
 				0 AS NovaraArchive,
                 'Facebook',
                 'Repost'
@@ -81,9 +82,10 @@ BEGIN
 	WHERE 		PageTypePrimary != 828 AND PageTypeSecondary != 828
 	AND			DATEDIFF(PublishDate,CAST(current_date() AS DATE))  IN (-3, -6)
 	ORDER BY	PublishDate DESC;
-	
-    INSERT INTO SocMediaStagingTable2 (PageID, PageTitle, PageTypePrimary, PageTypeSecondary, PublishDate, PostingDate, NovaraArchive, Platform, PostType)
+
+    INSERT INTO SocMediaStagingTable2 (PageID, RandID, PageTitle, PageTypePrimary, PageTypeSecondary, PublishDate, PostingDate, NovaraArchive, Platform, PostType)
 	SELECT 		PageID,
+				RandID,
 				PageTitle,
 				PageTypePrimary,
 				PageTypeSecondary,
@@ -93,19 +95,18 @@ BEGIN
                 'Twitter',
                 'Archive'
 	FROM		SocMediaStagingTable
-	WHERE 		PageTypePrimary = 2 
-	ORDER BY	RandID DESC
-	LIMIT		1;
+	WHERE 		PageTypePrimary = 2
+	ORDER BY	RandID DESC;
 
-	INSERT INTO SocMediaStagingTable3 (R, PageID, PageTitle, PageTypePrimary, PageTypeSecondary, PublishDate, NovaraArchive, Platform, PostType)
-	SELECT 		UUID() AS R, PageID, PageTitle, PageTypePrimary, PageTypeSecondary, PublishDate, NovaraArchive, Platform, PostType
+	INSERT INTO SocMediaStagingTable3 (RandID, PageID, PageTitle, PageTypePrimary, PageTypeSecondary, PublishDate, NovaraArchive, Platform, PostType)
+	SELECT 		RandID, PageID, PageTitle, PageTypePrimary, PageTypeSecondary, PublishDate, NovaraArchive, Platform, PostType
 	FROM 		SocMediaStagingTable2
-	ORDER BY 	UUID();
+	ORDER BY 	RandID
+	LIMIT		1;
     
     INSERT INTO WebsiteSocialMediaHistory (PageID, PageTitle, PageTypePrimary, PageTypeSecondary, PublishDate, PostingDate, NovaraArchive, Platform, PostType)
-	SELECT		PageID, PageTitle, PageTypePrimary, PageTypeSecondary, PublishDate, PostingDate, NovaraArchive, Platform, PostType
-	FROM		SocMediaStagingTable3
-	ORDER BY 	R;
+	SELECT		PageID, PageTitle, PageTypePrimary, PageTypeSecondary, PublishDate, CAST(current_date() AS DATE), NovaraArchive, Platform, PostType
+	FROM		SocMediaStagingTable3;
 
 	UPDATE 	WebsitePages w
 	SET		LastPosted = @CurrentDate,
